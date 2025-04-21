d4319b2c734c3586a9785c07d7ece7a39c9be9e7
Londongrad <h-b-12@mail.ru>
2025-04-21 21:27:52 +0900

    The beginning of implementing INotifyDataErrorInfo

---
+++ Flashcards-3.0/Flashcards.ViewModel/ErrorsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace Flashcards.ViewModels
+{
+    public class ErrorsViewModel : INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _propertyErrors;
+        public ErrorsViewModel()
+        {
+            errors = new Dictionary<string, List<string>>();
+        }
+        public bool HasErrors => throw new NotImplementedException();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
